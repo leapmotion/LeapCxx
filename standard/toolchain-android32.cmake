5d63964e032ec7e40320741981a762ab7d4d9d46
Hyoung Ham <hham@leapmotion.com>
2018-03-13 10:33:00 -0700

    remove a codition for CMAKE_ANDROID_ARCH_ABI and -mfpu option

--- standard/toolchain-android32.cmake
+++ standard/toolchain-android32.cmake
@@ -1,8 +1,7 @@
-set(CMAKE_ANDROID_ARCH_ABI armeabi)
+if(NOT DEFINED CMAKE_ANDROID_ARCH_ABI)
+  set(CMAKE_ANDROID_ARCH_ABI armeabi)
+endif()
 set(CMAKE_ANDROID_ARM_MODE ON)
-
-#Normally we would use   set(CMAKE_ANDROID_ARM_NEON ON), but it's not generally compatible with armv5te)
-set(CMAKE_CXX_FLAGS_INIT "-mfpu=neon")
 
 find_path(CMAKE_ANDROID_STANDALONE_TOOLCHAIN sysroot/usr/lib/libandroid.so PATHS
   /opt/android-standalone-toolchain
